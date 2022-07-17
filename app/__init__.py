0950703db2f449d373664093d06b9a8e3ec27919
ramesh <rameshrathiakg@gmail.com>
2022-07-17 15:11:14 +0530

    gd

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'super secret key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'mydatabase.db')
 db = SQLAlchemy(app)
 
 app.config.from_object(__name__)
